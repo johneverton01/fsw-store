cdec056140d1a5c0fd51d1d2dd4d869af6e738d2
John Everton <johneverton44@gmail.com>
2023-11-20 20:34:34 -0300

    feat: add computedProductTotalPRice

---
+++ src/helpers/products.ts
@@ -0,0 +1,21 @@
+import { Product } from "@prisma/client";
+
+interface ProductWithTotalPrice extends Product {
+  totalPrice: number;
+}
+
+export const computeProductTotalPrice = (
+  product: Product
+): ProductWithTotalPrice => {
+  if (product.discountPercent === 0) {
+    return {
+      ...product,
+      totalPrice: Number(product.basePrice),
+    };
+  }
+  const totalPrice = Number(product.basePrice) - Number(product.basePrice) * (product.discountPercent / 100);
+  return {
+    ...product,
+    totalPrice: Number(product.basePrice),
+  };
+};
