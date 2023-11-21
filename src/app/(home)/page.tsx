104b9d5b2aaedbb076aaa8abd0809f675176425b
John Everton <johneverton44@gmail.com>
2023-11-21 17:47:45 -0300

    feat: add second banner home

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -13,15 +13,32 @@ export default async function Home() {
   });
   return (
     <main className="flex min-h-screen flex-col gap-7 md:gap-10">
-      <Image className="h-auto w-full px-5" sizes="100vw" src="/banner-home-01.png" width={0} height={0} alt="até 55% desconto só esse mês" />
+      <Image 
+        className="h-auto w-full px-5" 
+        sizes="100vw" 
+        src="/banner-home-01.png" 
+        width={0} 
+        height={0} 
+        alt="até 55% desconto só esse mês"
+        />
 
       <div className="mt-8 px-5">
         <Categories />
       </div>
 
       <div className="mt-8">
+        <h2 className="mb-3 pl-5 font-bold uppercase">Ofertas</h2>
         <ProductList products={deals} />
       </div>
+
+      <Image 
+        className="h-auto w-full px-5" 
+        sizes="100vw" 
+        src="/banner-home-02.png" 
+        width={0} 
+        height={0} 
+        alt="até 55% desconto em mouses"
+        />
     </main>
   )
 }
